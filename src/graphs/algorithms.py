0054a68ca184c604670888b7afe503cf27f68c5b
PedrooSam <142419928+PedrooSam@users.noreply.github.com>
2025-10-27 14:39:43 -0300

    Implementação do algorítmo de Dijskra

--- src/graphs/algorithms.py
+++ src/graphs/algorithms.py
@@ -58,3 +58,43 @@ def dfs(grafo, inicio, visitados=None, ordem=None):
             dfs(grafo, vizinho, visitados, ordem)
 
     return ordem
+
+def dijkstra(grafo, inicio):
+
+    # Distâncias mínimas acumuladas
+    dist = {v: float('inf') for v in grafo}
+    dist[inicio] = 0
+
+    # Vértices visitados
+    visitados = set()
+
+    while len(visitados) < len(grafo):
+        # Seleciona o vértice com menor distância ainda não visitado
+        atual = None
+        menor_dist = float('inf')
+
+        #Percorre os vértices do grafo
+        for v in grafo:
+
+            #Caso o vértice ainda não tenha sido visitado e sua distância seja menor que a menor distância registrada, escolhe ele como o atual
+            if v not in visitados and dist[v] < menor_dist:
+                atual = v
+                menor_dist = dist[v]
+
+        #Caso não encontre um vértice atual, encerra o algorítmo
+        if atual is None:  
+            break
+
+        #Marca o vértice atual como visitado
+        visitados.add(atual)
+
+        #Atualiza distâncias dos vizinhos
+        for vizinho, peso in grafo[atual].items():
+
+            #Caso o vértice visinho não tenha sido fechado, atualiza a distância dele
+            if vizinho not in visitados:
+                nova_dist = dist[atual] + peso
+                if nova_dist < dist[vizinho]:
+                    dist[vizinho] = nova_dist
+
+    return dist
