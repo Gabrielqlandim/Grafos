80f5616ef0be529a8af0e8f985e6bffe7e41fc8c
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-11-16 09:50:13 -0300

    tratamento dataset

---
+++ parte2/filtrar_csv.py
@@ -0,0 +1,10 @@
+#Aqui é basico mas vou deixar comentado pra que todos entendam
+import pandas as pd
+#leio o dataset original que ta nessa pasta
+df = pd.read_csv("data/airlines_flights_data.csv")
+#filtro pegando so a SpiceJet
+df_spiceJet = df[df["airline"] == "SpiceJet"]
+#crio outro dataset e envio ele pra a pasta de out
+df_spiceJet.to_csv("out/airlines_spicejet.csv", index = False)
+#teste para ver se realemnte foi criado
+print("Arquivo criado")
