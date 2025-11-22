96f785064a5e9a50e6dae0d9b0f3885c3d41e813
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-11-22 18:24:24 -0300

    atualizações em dfs e bfs para a segunda parte:
    Ainda nao ta completo e falta o json e ajeitar a camada dps bfs mas o resto ta encaminhado

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-import parte2.main_2
+import src.parte2.main_2
--- src/parte2/main_2.py
+++ src/parte2/main_2.py
@@ -11,15 +11,45 @@ ordem, iteracoes = dfs(g, 'Delhi')
 fim_dfs = time.time()
 tempo_exec_dfs =fim_dfs - inicio_dfs
 
+print("DFS para Delhi")
+for posicao,vertice in enumerate(ordem, start=1):
+    print(f"A cidade {vertice} foi o {posicao}° da ordem de visita")
+
+print("\n")
+print(f"Houveram {iteracoes} camadas nesse DFS")
+print("\n")
+print(f"O tempo de execução desse DFS foi de: {tempo_exec_dfs}")
+print("\n")
+
 inicio_bfs = time.time()
-ordem_bfs, iteracoes_bfs = bfs(g, 'Mumbai')
+ordem_bfs, camadas_bfs = bfs(g, 'Mumbai')
 fim_bfs = time.time()
 tempo_exec_bfs =fim_bfs - inicio_bfs
 
+print("BFS para Mumbai")
+for posicao,vertice in enumerate(ordem, start=1):
+    print(f"A cidade {vertice} foi o {posicao}° da ordem de visita")
+
+print("\n")
+print(f"Houveram {camadas_bfs} camadas nesse BFS")
+print("\n")
+print(f"O tempo de execução desse DFS foi de: {tempo_exec_bfs}")
+print("\n")
+
 inicio_bfs2 = time.time()
-ordem_bfs2, iteracoes_bfs2 = bfs(g, 'Bangalore')
+ordem_bfs2, camadas_bfs2 = bfs(g, 'Bangalore')
 fim_bfs2 = time.time()
 tempo_exec_bfs2 = fim_bfs2 - inicio_bfs2
+
+print("BFS para Bangalore")
+for posicao,vertice in enumerate(ordem, start=1):
+    print(f"A cidade {vertice} foi o {posicao}° da ordem de visita")
+
+print("\n")
+print(f"Houveram {camadas_bfs2} camadas nesse BFS")
+print("\n")
+print(f"O tempo de execução desse DFS foi de: {tempo_exec_bfs2}")
+print("\n")
 
 #Parte de Dijskra
 
