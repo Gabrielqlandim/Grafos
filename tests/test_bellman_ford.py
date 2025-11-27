58b2054cfc3ccc1ce5396ee93933844ce9f76816
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2025-11-27 09:34:43 -0300

    bug no test de belman-ford resolvido

--- tests/test_bellman_ford.py
+++ tests/test_bellman_ford.py
@@ -2,8 +2,8 @@ from src.graphs.algorithms import bellman_ford
 
 def test_sem_ciclo():
     grafo_sem_ciclo = {
-        "A": {"B": [4], "C": [5]},
-        "B": {"D": [-2], "A":[4]},   
+        "A": {"B": [4,5], "C": [5]},
+        "B": {"D": [-2], "A":[4,5]},   
         "C": {"D": [1], "A": [5]},
         "D": {"B": [5], "C": [1]},   
     }
@@ -17,9 +17,9 @@ def test_sem_ciclo():
 
 def test_com_ciclo():
     grafo_com_ciclo = {
-        "A": {"B": 1},
-        "B": {"C": -1},
-        "C": {"A": -1},
+        "A": {"B": [1], "C": [-1]},
+        "B": {"C": [-1,-3],"A": [1]},
+        "C": {"A": [-1], "B": [-1,-3]},
     }
 
     resultado = bellman_ford(grafo_com_ciclo, "A")
